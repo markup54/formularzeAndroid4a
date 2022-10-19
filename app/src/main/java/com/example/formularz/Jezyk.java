399a9f92ed4f51d5bf3681d4e61fa56ebb4222fe
markup54 <marzenakupka@tlen.pl>
2022-10-19 07:28:47 +0200

    formularze

---
+++ app/src/main/java/com/example/formularz/Jezyk.java
@@ -0,0 +1,2 @@
+package com.example.formularz;public class Jezyk {
+}
---
+++ app/src/main/java/com/example/formularz/MainActivity4P.java
@@ -0,0 +1,14 @@
+package com.example.formularz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity4P extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_4p);
+    }
+}
